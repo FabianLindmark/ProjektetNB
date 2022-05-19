f493def3f8e51c99d4e86f4f57d6ab1f0a3f1b13
portuguesellen <105210458+portuguesellen@users.noreply.github.com>
2022-05-19 17:34:53 +0200

    e

---
+++ Mib/src/mib/Validering.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mib;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author ellenportugues
+ */
+public class Validering {
+    
+    public static boolean textFaltHarVarde(JTextField rutaAttKolla)
+    {
+        boolean resultat = true; 
+        
+        if(rutaAttKolla.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Inmatningsrutan är tom");
+            resultat = false;
+            
+        }
+        
+        return resultat;
+        
+    }
+    
+    //metod för fält som ska vara int, 
+     public static boolean isHeltal (JTextField rutaAttKolla){
+        
+        boolean resultat = true;
+        
+        try {
+            String inString = rutaAttKolla.getText();
+            Integer.parseInt(inString);
+            rutaAttKolla.requestFocus();
+        }
+        catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Var god ange ett heltal!");
+            resultat = false;
+        }
+        
+        
+         return resultat;
+    }
+     
+     /**metod för fält som ska vara String
+     public static boolean 
+          
+     boolean resultat = true;
+     
+     try{
+    
+        
+    
+        }
+     catch(){
+    
+    
+        }
+     
+    return resultat;
+    **/
+    
+}
